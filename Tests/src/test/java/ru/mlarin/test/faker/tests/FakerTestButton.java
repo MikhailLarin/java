b64ce7930ece052987c5aa2bd0a283460b9d399c
Mikhail Larin <mlarin@smartlabs.tv>
2018-02-22 17:35:39 +0300

    внесены изменения

---
+++ Tests/src/test/java/ru/mlarin/test/faker/tests/FakerTestButton.java
@@ -0,0 +1,15 @@
+package ru.mlarin.test.tests;
+
+import org.testng.annotations.Test;
+
+public class FakerTestButton extends FakerTestBase {
+
+  @Test
+  public void testFaker() throws Exception {
+    app.isElementEnabled();
+
+  }
+
+
+
+}
