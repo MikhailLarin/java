2144c0aea0a4d8b22f5929056a8629cf592deb83
Mikhail Larin <mlarin@smartlabs.tv>
2018-03-02 18:27:21 +0300

    Внесены изменения

--- Instagram/src/test/java/ru/nakrutka/instagram/appmanager/RegistrationHelper.java
+++ Instagram/src/test/java/ru/nakrutka/instagram/appmanager/RegistrationHelper.java
@@ -1,18 +1,20 @@
 package ru.nakrutka.instagram.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-    private final ApplicationManager app;
-    private WebDriver driver;
+public class RegistrationHelper extends HelperBase {
+
+
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        driver =app.getDriver();
+        super(app);
     }
 
     public void start(String username, String email) {
-        driver.get(app.getProperty("web.baseUrl")+"/?hl=ru");
-        driver.findElement(By.id("fee8b93ef1faf8")).sendKeys(email);
+        driver.get(app.getProperty("web.baseUrl"));
+        type(By.name("emailOrPhone"),email);
+        driver.findElement(By.name("fullName")).sendKeys("Nakrutka Instagram");
+        type(By.name("username"),username);
+        driver.findElement(By.name("password")).sendKeys("PAsssWord99");
+        click(By.xpath("//*[@id=\"react-root\"]/section/main/article/div[2]/div[1]/div/form/div[6]/span/button"));
     }
 }
